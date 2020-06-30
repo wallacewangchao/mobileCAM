8cdc58ce5c9a68dbf497d22102a4bd0c9ac32a19
Chao Wang <chao.wang@honda-ri.de>
2020-07-01 00:29:57 +0200

    try to get rid of check mediaDevices.enumerateDevices for compatiable with Wechat

--- js/c_main.js
+++ js/c_main.js
@@ -68,9 +68,10 @@ function deviceCount() {
 document.addEventListener('DOMContentLoaded', function (event) {
   // check if mediaDevices is supported
   if (
-      navigator.mediaDevices &&
-      navigator.mediaDevices.getUserMedia &&
-      navigator.mediaDevices.enumerateDevices
+      navigator.mediaDevices 
+      // &&
+      // navigator.mediaDevices.getUserMedia &&
+      // navigator.mediaDevices.enumerateDevices
   ) {
       // first we call getUserMedia to trigger permissions
       // we need this before deviceCount, otherwise Safari doesn't return all the cameras
